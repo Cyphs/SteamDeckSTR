4a05b77a7589ee0a3e1335d793b9c9811c0cf67a
X <119499221+Cyphs@users.noreply.github.com>
2024-03-29 14:20:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ mkdir -p ~/.local/share/applications/
 
 mkdir -p ~/.Cyphs
 cd ~/.Cyphs
-wget -O SteamDeckSTR.zip https://github.com/Cyphs/SteamDeckSTR/archive/refs/heads/master.zip
+wget -O SteamDeckSTR.zip https://github.com/Cyphs/SteamDeckSTR/archive/refs/heads/tests.zip
 unzip -o SteamDeckSTR.zip
 rm SteamDeckSTR.zip
 
-~/.Cyphs/SteamDeckSTR-master/post-install.sh
+~/.Cyphs/SteamDeckSTR-tests/post-install.sh
 
 echo "Success! Exiting in 3..."
 sleep 3
